8de2d421dca12b345faee110b191907a92863e71
Shihad <34054298+shihadsalam@users.noreply.github.com>
2018-06-19 19:01:33 +0530

    Update MyConstants.java

--- MySpringBoot/src/main/java/com/springboot/application/MyConstants.java
+++ MySpringBoot/src/main/java/com/springboot/application/MyConstants.java
@@ -19,4 +19,8 @@ public class MyConstants {
 	
 	public static final String DEL_EMP_API_DOC_NOTES = "It will delete the employee specified in the request.";
 	
+	public static final String UPD_EMP_API_DOC_VALUE = "Update the specified employee";
+	
+	public static final String UPD_EMP_API_DOC_NOTES = "It will update the employee specified in the request.";
+	
 }
